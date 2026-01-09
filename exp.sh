0f195eafb9cfb485053443f577181241302561d5
Zhengqi Gao <zhengqi@mit.edu>
2026-01-08 21:45:21 -0500

    update

--- exp.sh
+++ exp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 1
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 2
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 3
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 4
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 5
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 6
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 7
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 8
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 9
-python main.py --epochs 5000 --lr 0.005 --save_best --nn_type BasicFNN --batch_size 32  --seed 10
+python main.py --epochs 20000 --lr 0.001  --save_best --nn_type SpectralCNN --batch_size 64  --seed 1
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 2
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 3
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 4
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 5
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 6
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 7
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 8
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 9
+python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 10
 
 python kaggle_submit.py
