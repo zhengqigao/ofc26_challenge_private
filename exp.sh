8779d5656acfe0219cba757c69bfe3d4dde2e92d
Zhengqi Gao <zhengqi@mit.edu>
2026-01-08 23:12:13 -0500

    update

--- exp.sh
+++ exp.sh
@@ -5,10 +5,6 @@ python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --bat
 python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 3
 python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 4
 python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 5
-python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 6
-python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 7
-python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 8
-python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 9
-python main.py --epochs 20000 --lr 0.001 --save_best --nn_type SpectralCNN --batch_size 64  --seed 10
+
 
 python kaggle_submit.py
