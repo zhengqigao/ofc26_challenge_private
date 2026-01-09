342519fb90525d383bf1160a12715ef3683c48b7
Zhengqi Gao <zhengqi@mit.edu>
2026-01-09 11:11:04 -0500

    update

--- main_zq.py
+++ main_zq.py
@@ -176,6 +176,17 @@ if __name__ == "__main__":
             dropout=0.1,
             spectra_noise_std=0.0,
             global_noise_std=0.0,)
+    elif args.nn_type == "ImprovedEmbedDeepSpectralCNN":
+        base_model = ImprovedEmbedDeepSpectralCNN(
+            X_tensor.shape[1],
+            output_dim = 95,
+            num_channels = 95,
+            global_dim=4,
+            hidden_channels=32,
+            hidden_embed_dim=4,
+            dropout=0.1,
+            spectra_noise_std=0.0,
+            global_noise_std=0.0,)
     else:
         raise ValueError(f"Invalid nn_type: {args.nn_type}.")
 
