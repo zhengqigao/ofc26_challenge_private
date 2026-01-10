79cf46b53f92236e2004f5840ba001454e4c2673
Zhengqi Gao <zhengqi@mit.edu>
2026-01-09 23:05:09 -0500

    update

--- kaggle_submit.py
+++ kaggle_submit.py
@@ -1,14 +1,29 @@
 import os 
 import time
-submission_dir = "./submission"
+import argparse
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--remove_if_submit_success", action="store_true", default=False)
+    args = parser.parse_args()
+    submission_dir = "./submission"
 
-count = 0
-# Iterate through all files in the submission folder
-for file in os.listdir(submission_dir):
-    if file.endswith(".csv"):
-        file_path = os.path.abspath(os.path.join(submission_dir, file))
-        command = f"kaggle competitions submit -c ofc-2026-ml-challenge -f {file_path} -m message"
-        os.system(command)
-        count += 1
-    if count % 10 == 0:
-        time.sleep(20)
+    count = 0
+    # Iterate through all files in the submission folder
+    submit_fail_list = []
+    for file in os.listdir(submission_dir):
+        if file.endswith(".csv"):
+            file_path = os.path.abspath(os.path.join(submission_dir, file))
+            command = f"kaggle competitions submit -c ofc-2026-ml-challenge -f {file_path} -m message"
+            try:
+                return_flag =os.system(command)
+            except Exception as e:
+                print(f"Error submitting {file}: {e}")
+                submit_fail_list.append(file)
+            if return_flag == 0:
+                print(f"Submit success: {file}")
+                if args.remove_if_submit_success:
+                    os.remove(file_path)
+                count += 1
+        if count % 10 == 0:
+            time.sleep(20)
+    print(f"Submit fail list: {submit_fail_list}")
